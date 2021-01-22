6082691b8a84b5cc39140160f807405237f66079
Brendan Bond <brendanbond@gmail.com>
2021-01-22 16:37:36 -0600

    2000 points on test product

--- src/handlers/handleNewOrderWebhookRequest.ts
+++ src/handlers/handleNewOrderWebhookRequest.ts
@@ -59,7 +59,7 @@ export const handleNewOrderWebhookRequest = async (
       process.env.NODE_ENV === 'development' &&
       lineItemsContainTestProduct(lineItems)
     ) {
-      newPoints = 200;
+      newPoints = 2000;
     } else {
       newPoints = Math.floor(Number(subTotal));
     }
