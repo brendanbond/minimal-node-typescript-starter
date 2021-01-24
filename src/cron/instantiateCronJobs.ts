a164da1a52c4aaeb8cb75d0b755f842464dbacbf
Brendan Bond <brendanbond@gmail.com>
2021-01-23 23:39:20 -0600

    change vesting times

--- src/cron/instantiateCronJobs.ts
+++ src/cron/instantiateCronJobs.ts
@@ -2,7 +2,7 @@ import cron from 'node-cron';
 
 import { vestEligiblePoints } from './vestEligiblePointsAndUpdateGiftEligibility';
 
-const cronJobs = [() => cron.schedule('* * * * * *', vestEligiblePoints)];
+const cronJobs = [() => cron.schedule('* * * * *', vestEligiblePoints)];
 
 export const instantiateCronJobs = () => {
   cronJobs.forEach((cronJobFn) => cronJobFn());
--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,8 +2,8 @@ export const constants: {
   vestTimeAmount: number;
   vestTimeUnit: 'second' | 'minute' | 'day';
 } = {
-  vestTimeUnit: 'second',
-  vestTimeAmount: 20,
+  vestTimeUnit: 'minute',
+  vestTimeAmount: 10,
 };
 
 export const ROOT_ENDPOINT = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/${process.env.SHOPIFY_API_VERSION}`;
