91fea1939c28919b4a5cfc5b53b0a64db7307453
Brendan Bond <brendanbond@gmail.com>
2021-03-05 23:29:03 -0600

    ready for launch with 40 day vesting period

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -2,8 +2,8 @@ export const constants: {
   vestTimeAmount: number;
   vestTimeUnit: 'second' | 'minute' | 'day';
 } = {
-  vestTimeUnit: 'minute',
-  vestTimeAmount: 1,
+  vestTimeUnit: 'day',
+  vestTimeAmount: 40,
 };
 
 export const ROOT_ENDPOINT = `https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@${process.env.SHOPIFY_SHOP_NAME}.myshopify.com/admin/api/${process.env.SHOPIFY_API_VERSION}`;
